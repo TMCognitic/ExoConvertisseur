e8b5d72ad7101320cbfe4b81a68abe5473898cb5
TMCognitic <thierry.morre@cognitic.be>
2021-12-07 16:59:30 +0100

    Ajoutez des fichiers projet.

---
+++ ExoConvertisseur/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExoConvertisseur
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long value;
+
+            Console.Write("Entrez le nombre à convertir : ");
+            while (!long.TryParse(Console.ReadLine(), out value))
+            {
+                Console.Clear();
+                Console.Write("Entrez le nombre à convertir : ");
+            }
+
+            for (int i = 2; i < 37; i++)
+            {
+                Console.WriteLine($"base {i:D2} : {Convert(value, i)}");
+            }
+        }
+
+        static string Convert(long value, int baseToConvert)
+        {
+            if (baseToConvert < 2 || baseToConvert > 36)
+                return null;
+
+            string baseValues = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";            
+            string result = "";
+
+            int modulo = 0;
+
+            while (value >= baseToConvert)
+            {
+                modulo = (int)(value % baseToConvert);
+                value /= baseToConvert;
+                result = baseValues[modulo] + result;
+            }
+
+            result = baseValues[(int)value] + result;
+
+            return result;
+        }
+    }
+
+}
